4418068922dbf97c5a70bfd2ec967f7c2e02e811
Dmitry Kim <d.kim2009@tut.by>
2019-10-11 23:19:42 +0300

    feat: guessing-games task is solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = {};
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.range = {
+            min:min,
+            max:max
+        }
     }
 
     guess() {
-
+        this.result = Math.round(this.range.min+(this.range.max-this.range.min)/2);
+        return this.result;
     }
 
     lower() {
-
+        this.range.max = this.result;
     }
 
     greater() {
-
+        this.range.min = this.result;
     }
 }
 
